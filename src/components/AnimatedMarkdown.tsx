e3c19282a4b48bc86a23c16225f918a374ea7c18
Evan Phibbs <evan.phibbs@gmail.com>
2024-11-05 00:30:58 -0800

    updated version

--- src/components/AnimatedMarkdown.tsx
+++ src/components/AnimatedMarkdown.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm'
-import rehypeRaw from "rehype-raw";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import style from 'react-syntax-highlighter/dist/esm/styles/hljs/docco'
 import './animations.css';
